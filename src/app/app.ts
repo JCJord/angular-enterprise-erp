9afd99f4f78be051ea7f44bcc910a15b31c80532
Julio Jordão <juliojc.jord@gmail.com>
2026-09-06 20:50:01 -0300

    chore: hero icons depedency and config

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,33 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { 
+  heroPlusSolid, 
+  heroPencilSquareSolid, 
+  heroTrashSolid, 
+  heroPrinterSolid, 
+  heroFunnelSolid,
+  heroBuildingStorefrontSolid,
+  heroTruckSolid,
+  heroCheckCircleSolid
+} from '@ng-icons/heroicons/solid';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NgIconComponent],
+  providers: [
+    provideIcons({
+      heroPlusSolid,
+      heroPencilSquareSolid,
+      heroTrashSolid,
+      heroPrinterSolid,
+      heroFunnelSolid,
+      heroBuildingStorefrontSolid,
+      heroTruckSolid,
+      heroCheckCircleSolid
+    })
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
