cc13f180776b26528100853d51559eb4af71797c
Julio Jordão <juliojc.jord@gmail.com>
2026-09-09 01:06:30 -0300

    chore: add barrel import for auth module

---
+++ src/app/core/auth/index.ts
@@ -0,0 +1,5 @@
+﻿export * from './models/auth.models';
+export * from './services/auth.service';
+export * from './interceptors/jwt.interceptor';
+export * from './guards/auth.guard';
+export * from './guards/guest.guard';
