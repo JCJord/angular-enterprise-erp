a9d11560e0631fcabe69da08582f0d35f49af9d1
Julio Jordão <juliojc.jord@gmail.com>
2026-09-09 01:05:29 -0300

    chore: setup auth guard, guest guard and auth routes into app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
-import { Routes } from '@angular/router';
+﻿import { Routes } from '@angular/router';
+import { authGuard, guestGuard } from './core/auth';
+import { AppLayoutComponent } from './core/layout/app-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/auth.routes').then(m => m.AUTH_ROUTES),
+    canActivate: [guestGuard]
+  },
+  {
+    path: '',
+    component: AppLayoutComponent,
+    canActivate: [authGuard],
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./features/dashboard/dashboard.routes').then(m => m.DASHBOARD_ROUTES)
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./features/dashboard/dashboard.routes').then(m => m.DASHBOARD_ROUTES)
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
