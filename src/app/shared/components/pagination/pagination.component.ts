7779bd60ae1db02a7e108d8db24da97b536fdbfc
Julio Jordão <juliojc.jord@gmail.com>
2026-09-18 15:52:14 -0300

    feat: add pagination component

---
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -0,0 +1,59 @@
+import { Component, computed, input, output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import {
+  heroChevronLeftSolid,
+  heroChevronRightSolid
+} from '@ng-icons/heroicons/solid';
+import { environment } from '../../../../environments/environment';
+
+@Component({
+  selector: 'app-pagination',
+  standalone: true,
+  imports: [CommonModule, NgIconComponent],
+  providers: [
+    provideIcons({
+      heroChevronLeftSolid,
+      heroChevronRightSolid
+    })
+  ],
+  templateUrl: './pagination.component.html',
+  styleUrl: './pagination.component.scss'
+})
+export class PaginationComponent {
+  page = input<number>(1);
+  pageSize = input<number>(environment.defaultPageSize);
+  total = input<number>(0);
+  pageSizeOptions = input<number[]>(environment.pageSizeOptions);
+  showPageSize = input<boolean>(true);
+  showTotal = input<boolean>(true);
+
+  totalPages = computed(() =>
+    Math.max(1, Math.ceil(this.total() / (this.pageSize() || 1)))
+  );
+
+  hasPrevious = computed(() => this.page() > 1);
+  hasNext = computed(() => this.page() < this.totalPages());
+
+  pageChange = output<number>();
+  pageSizeChange = output<number>();
+
+  onPrevious(): void {
+    if (this.hasPrevious()) {
+      this.pageChange.emit(this.page() - 1);
+    }
+  }
+
+  onNext(): void {
+    if (this.hasNext()) {
+      this.pageChange.emit(this.page() + 1);
+    }
+  }
+
+  onPageSizeSelect(event: Event): void {
+    const val = Number((event.target as HTMLSelectElement).value);
+    if (val && val !== this.pageSize()) {
+      this.pageSizeChange.emit(val);
+    }
+  }
+}
