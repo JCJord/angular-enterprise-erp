7f8fbef282ddf50dcd17d370a700ca3a2ab71107
Julio Jordão <juliojc.jord@gmail.com>
2026-09-18 20:44:01 -0300

    feat: add jewerly details dialog component

---
+++ src/app/features/inventory/components/jewelry-details-dialog/jewelry-details-dialog.component.ts
@@ -0,0 +1,60 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
+import { JewelryCategoryLabels, JewelryItem, MetalTypeLabels } from '../../models/inventory.model';
+import { CurrencyBrPipe, JewelryWeightPipe } from '../../../../shared/pipes';
+import { ButtonComponent } from '../../../../shared/components';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { heroCubeSolid, heroPencilSquareSolid, heroXMarkSolid } from '@ng-icons/heroicons/solid';
+
+export interface JewelryDetailsDialogData {
+  item: JewelryItem;
+}
+
+@Component({
+  selector: 'app-jewelry-details-dialog',
+  standalone: true,
+  imports: [
+    CommonModule,
+    CurrencyBrPipe,
+    JewelryWeightPipe,
+    ButtonComponent,
+    NgIconComponent
+  ],
+  providers: [
+    provideIcons({
+      heroCubeSolid,
+      heroPencilSquareSolid,
+      heroXMarkSolid
+    })
+  ],
+  templateUrl: './jewelry-details-dialog.component.html',
+  styleUrl: './jewelry-details-dialog.component.scss'
+})
+export class JewelryDetailsDialogComponent {
+  readonly dialogRef = inject(DialogRef);
+  readonly data = inject<JewelryDetailsDialogData>(DIALOG_DATA);
+
+  get item(): JewelryItem {
+    return this.data.item;
+  }
+
+  get categoryLabel(): string {
+    return JewelryCategoryLabels[this.item.category] || this.item.category;
+  }
+
+  get metalLabel(): { label: string; badgeClass: string } {
+    return MetalTypeLabels[this.item.metal_type] || {
+      label: String(this.item.metal_type),
+      badgeClass: 'bg-app text-secondary border-base'
+    };
+  }
+
+  onClose(): void {
+    this.dialogRef.close();
+  }
+
+  onEdit(): void {
+    this.dialogRef.close('edit');
+  }
+}
