381319e87a9c501958105d0b09b3012deded18fd
Julio Jordão <juliojc.jord@gmail.com>
2026-09-18 20:44:31 -0300

    feat: add confirm dialog component

---
+++ src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,40 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
+import { ButtonComponent, ButtonVariant } from '../button/button.component';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { heroExclamationTriangleSolid, heroXMarkSolid } from '@ng-icons/heroicons/solid';
+
+export interface ConfirmDialogData {
+  title: string;
+  message: string;
+  confirmText?: string;
+  cancelText?: string;
+  variant?: ButtonVariant;
+}
+
+@Component({
+  selector: 'app-confirm-dialog',
+  standalone: true,
+  imports: [CommonModule, ButtonComponent, NgIconComponent],
+  providers: [
+    provideIcons({
+      heroExclamationTriangleSolid,
+      heroXMarkSolid
+    })
+  ],
+  templateUrl: './confirm-dialog.component.html',
+  styleUrl: './confirm-dialog.component.scss'
+})
+export class ConfirmDialogComponent {
+  readonly dialogRef = inject(DialogRef);
+  readonly data = inject<ConfirmDialogData>(DIALOG_DATA);
+
+  onConfirm(): void {
+    this.dialogRef.close(true);
+  }
+
+  onCancel(): void {
+    this.dialogRef.close(false);
+  }
+}
